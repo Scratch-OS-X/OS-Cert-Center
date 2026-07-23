69533b1f01461af7d5727b25a6d916f7d56b4ae0
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-07-23 16:21:22 +0200

    Create qr-codes.py

---
+++ qr-codes.py
@@ -0,0 +1,23 @@
+import platform
+import sys
+
+try:
+    import qrcode
+except ImportError:
+    if platform.system() == "Windows":
+        cmd = 'pip install "qrcode[pil]"'
+    else:
+        cmd = 'pip3 install "qrcode[pil]"'
+
+    print(f"Erreur : La bibliothèque 'qrcode' n'est pas installée.")
+    print(f"Veuillez l'installer en exécutant la commande suivante :\n")
+    print(f"    {cmd}\n")
+    sys.exit(1)
+
+# Le reste de votre code
+data = "LHF-557-vvm"
+
+qr = qrcode.QRCode(version=1, box_size=10, border=4)
+qr.add_data(data)
+qr.make(fit=True)
+qr.print_ascii(invert=True)
