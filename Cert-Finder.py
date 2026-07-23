2dcf1d020c9d48a08c929089c71f9baf8f1a51af
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-07-23 16:38:14 +0200

    Update Cert-Finder.py

--- Cert-Finder.py
+++ Cert-Finder.py
@@ -16,8 +16,5 @@ if re.fullmatch(r"^[A-Z]{3}-\d{3}-[a-z]{3}$", Certificat):
      print(f"Certificat n° \033[1m{Certificat}\033[0m \033[33m certificat exemple \033[0m")
   else :
      print(f"\033[31m ✖ Le certificat \033[1m{Certificat}\033[m \033[31m n'existe pas.\033[0m")
-elif Certificat == "ADMIN" :
-  print("ADMIN Mode")
-  ADMIN = input("Administration >>> ")
 else:
     print("\033[31m ✖ Format invalide. Format correct : \033[1m ABC-123-def \033[0m \033[0m")
