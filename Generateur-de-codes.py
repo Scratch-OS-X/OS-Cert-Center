8b38419a51cb44981a0b042afab6c79b46a79ddb
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-07-23 16:29:21 +0200

    Update Generateur-de-codes.py

--- Generateur-de-codes.py
+++ Generateur-de-codes.py
@@ -1,10 +1,32 @@
 import random
 import string
 
+try:
+    import qrcode
+except ImportError:
+    if platform.system() == "Windows":
+        cmd = 'pip install "qrcode[pil]"'
+    else:
+        cmd = 'pip3 install "qrcode[pil]"'
+
+    print("Erreur : La bibliothèque 'qrcode' n'est pas installée.")
+    print("Veuillez l'installer en exécutant la commande suivante :\n")
+    print(f"    {cmd}\n")
+    sys.exit(1)
+
+def afficher_qr(data):
+    """Affiche un QR code ASCII directement dans le terminal."""
+    qr = qrcode.QRCode(version=1, box_size=10, border=4)
+    qr.add_data(data)
+    qr.make(fit=True)
+    qr.print_ascii(invert=True)
+
 def generer_code():
     ABC = ''.join(random.choice(string.ascii_uppercase) for _ in range(3))
     _123 = ''.join(random.choice(string.digits) for _ in range(3))
     ghi = ''.join(random.choice(string.ascii_lowercase) for _ in range(3))
+    data = f"{ABC}-{_123}-{ghi}"
     return f"{ABC}-{_123}-{ghi}"
 
 print(generer_code())
+print(afficher_qr(data))
