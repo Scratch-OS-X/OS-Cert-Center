43b3eaf809c056ca413cb7e310f9602d717cbebf
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-07-23 16:30:59 +0200

    Update Generateur-de-codes.py

--- Generateur-de-codes.py
+++ Generateur-de-codes.py
@@ -1,5 +1,7 @@
+import platform
 import random
 import string
+import sys
 
 try:
     import qrcode
@@ -14,6 +16,7 @@ except ImportError:
     print(f"    {cmd}\n")
     sys.exit(1)
 
+
 def afficher_qr(data):
     """Affiche un QR code ASCII directement dans le terminal."""
     qr = qrcode.QRCode(version=1, box_size=10, border=4)
@@ -21,12 +24,14 @@ def afficher_qr(data):
     qr.make(fit=True)
     qr.print_ascii(invert=True)
 
+
 def generer_code():
-    ABC = ''.join(random.choice(string.ascii_uppercase) for _ in range(3))
-    _123 = ''.join(random.choice(string.digits) for _ in range(3))
-    ghi = ''.join(random.choice(string.ascii_lowercase) for _ in range(3))
-    data = f"{ABC}-{_123}-{ghi}"
+    ABC = "".join(random.choice(string.ascii_uppercase) for _ in range(3))
+    _123 = "".join(random.choice(string.digits) for _ in range(3))
+    ghi = "".join(random.choice(string.ascii_lowercase) for _ in range(3))
     return f"{ABC}-{_123}-{ghi}"
 
-print(generer_code())
-print(afficher_qr(data))
+
+code_genere = generer_code()
+print(code_genere)
+afficher_qr(code_genere)
